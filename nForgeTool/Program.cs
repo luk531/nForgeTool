e6bcfb992f4f6e82ee862b1c5399c2ff70323aaa
Luk <Luk@LUK-PC>
2018-08-08 04:19:24 +0200

    new

--- nForgeTool/Program.cs
+++ nForgeTool/Program.cs
@@ -15,7 +15,7 @@ namespace nForgeTool
         static void Main()
         {
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            Application.SetCompatibleTextRenderingDefault(true);
             Application.Run(new Form1());
         }
     }
