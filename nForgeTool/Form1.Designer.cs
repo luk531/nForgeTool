a3791e9853ba20b8fab20031c3b290338307ab36
Luk <Luk@LUK-PC>
2018-08-08 16:23:46 +0200

    new

--- nForgeTool/Form1.Designer.cs
+++ nForgeTool/Form1.Designer.cs
@@ -28,20 +28,69 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.buttonStartStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pictureBox1.Location = new System.Drawing.Point(4, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(935, 702);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(4, 730);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(935, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // buttonStartStop
+            // 
+            this.buttonStartStop.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStartStop.Location = new System.Drawing.Point(4, 757);
+            this.buttonStartStop.Name = "buttonStartStop";
+            this.buttonStartStop.Size = new System.Drawing.Size(500, 23);
+            this.buttonStartStop.TabIndex = 2;
+            this.buttonStartStop.Text = "& Start / Stop";
+            this.buttonStartStop.UseVisualStyleBackColor = true;
+            this.buttonStartStop.Click += new System.EventHandler(this.buttonStartStop_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(941, 884);
+            this.ClientSize = new System.Drawing.Size(951, 791);
+            this.Controls.Add(this.buttonStartStop);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
+            this.ShowIcon = false;
             this.Text = "Video Capture";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button buttonStartStop;
     }
 }
 
--- nForgeTool/Form1.cs
+++ nForgeTool/Form1.cs
@@ -8,13 +8,65 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using AForge.Video;
+using AForge.Video.DirectShow;
+
 namespace nForgeTool
 {
     public partial class Form1 : Form
     {
+        private FilterInfoCollection _videoDevices;
+        private VideoCaptureDevice _videoCaptureDevice;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            foreach (FilterInfo device in _videoDevices)
+            {
+                comboBox1.Items.Add(device.Name);
+            }
+
+            _videoCaptureDevice = new VideoCaptureDevice();
+
+
+        }
+
+        private void buttonStartStop_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == -1)
+                return;
+
+            if (_videoCaptureDevice.IsRunning)
+            {
+                _videoCaptureDevice.Stop();
+                pictureBox1.Image = null;
+                pictureBox1.Invalidate();
+            }
+            else
+            {
+                _videoCaptureDevice = new VideoCaptureDevice(_videoDevices[comboBox1.SelectedIndex].MonikerString);
+                // set NewFrame event handler
+                _videoCaptureDevice.NewFrame += _videoCaptureDevice_NewFrame;
+                _videoCaptureDevice.Start();
+            }
+        }
+
+        private void _videoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            Bitmap image = (Bitmap)eventArgs.Frame.Clone();
+            pictureBox1.Image = image;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (_videoCaptureDevice.IsRunning)
+                _videoCaptureDevice.Stop();
         }
     }
 }
